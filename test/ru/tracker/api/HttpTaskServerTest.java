9b4c1ce50f80fbee6404963db1393698f377d89f
Kirill Kapustin <kanyct04ka@yandex.ru>
2025-07-03 15:41:43 +0300

    fixed: checking server and handlers running

--- test/ru/tracker/api/HttpTaskServerTest.java
+++ test/ru/tracker/api/HttpTaskServerTest.java
@@ -9,11 +9,20 @@ import ru.tracker.api.adapters.DurationAdapter;
 import ru.tracker.api.adapters.LocalDateTimeAdapter;
 import ru.tracker.controller.Managers;
 import ru.tracker.controller.TaskManager;
+import ru.tracker.model.Task;
+import ru.tracker.model.TaskStatus;
 
 import java.io.IOException;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HttpTaskServerTest {
     protected TaskManager taskManager = Managers.getDefault();
@@ -40,4 +49,72 @@ public class HttpTaskServerTest {
         taskServer.stop();
     }
 
+    // проверка, что сервер в принципе запущен и работает
+    @Test
+    public void checkRunningServer() throws IOException, InterruptedException {
+
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI uri = URI.create("http://localhost:8080/");
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(404, response.statusCode());
+        }
+    }
+
+    // далее проверки, что основные обработчики работают
+    @Test
+    public void checkTaskHandler() throws IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI uri = URI.create("http://localhost:8080/tasks");
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(200, response.statusCode());
+        }
+    }
+
+    @Test
+    public void checkEpicHandler() throws IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI uri = URI.create("http://localhost:8080/epics");
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(200, response.statusCode());
+        }
+    }
+
+    @Test
+    public void checkSubtaskHandler() throws IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI uri = URI.create("http://localhost:8080/subtasks");
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(200, response.statusCode());
+        }
+    }
+
+    @Test
+    public void checkHistoryHandler() throws IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI uri = URI.create("http://localhost:8080/history");
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(200, response.statusCode());
+        }
+    }
+
+    @Test
+    public void checkPrioritizedHandler() throws IOException, InterruptedException {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            URI uri = URI.create("http://localhost:8080/prioritized");
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(uri).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            assertEquals(200, response.statusCode());
+        }
+    }
 }
